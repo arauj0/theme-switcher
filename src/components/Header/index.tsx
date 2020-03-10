0b0dc9306777462d51112b967fe54ce8345ff73f
Larissa Araújo <lariaraujo201@gmail.com>
2020-03-10 13:45:02 -0300

    Adicionada a função para mudar o state do mode para dark ou light.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,16 +5,20 @@ import { shade } from 'polished';
 
 import { Container } from './styles';
 
-const Header: React.FC = () => {
-    const { colors } = useContext(ThemeContext);
+interface Props {
+    toggleTheme(): void;
+}
+
+const Header: React.FC<Props> = ({ toggleTheme }) => {
+    const { colors, title } = useContext(ThemeContext);
 
     return (
         <Container>
             Hello World
 
             <Switch
-                onChange={() => {}}
-                checked={true}
+                onChange={toggleTheme}
+                checked={title === 'dark'}
                 checkedIcon={false}
                 uncheckedIcon={false}
                 height={10}
